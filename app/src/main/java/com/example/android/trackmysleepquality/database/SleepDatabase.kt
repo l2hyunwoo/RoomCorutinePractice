15e1374f3f01269251abce42588ad8bbfe6d7e73
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-11-10 17:09:45 +0900

    feat: Make appdatabase

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,45 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+//1. RoomDatabase를 상속하는 추상클래스를 만든다 + @Database
+// exportScheme 공부(추가조사)
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+    // 2. DAO를 추상 Property로 선언
+    // Table만큼 Dao 선언
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+        // 동일한 스레드에서 계속 작동됨을 보장
+        // 변수가 절대로 Cache되지 않음
+        // 메인 메모리에서 관리
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            // 멀티스레드 환경에서 DB 동시 접근 가능
+            // 이 코드는 한 스레드만 접근 가능
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep history database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
